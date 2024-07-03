78ae38fcdb0723fec8f47488e9aa4266ee237729
최성현 <popopqo777@gmail.com>
2024-07-03 19:58:00 +0900

    fix : link 2 front

--- src/main/java/OAuth/practice/config/auth/WebConfig.java
+++ src/main/java/OAuth/practice/config/auth/WebConfig.java
@@ -21,7 +21,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000",
+                        "222.109.149.187:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
